10f7440a1bf02fa090f6149216b9a162fa04671c
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2024-01-29 15:07:28 +0200

    contactList style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,13 @@
+import css from './ContactList.module.css';
+
 export const ContactListItem = ({ id, name, number }) => {
   return (
-    <div className="itemBox">
-      <p className="name">{name}</p>
-      <p className="number">{number}</p>
-      <button className="id">Delete{id}</button>
+    <div className={css.itemBox}>
+      <div className={css.leftBox}>
+        <p className={css.name}>{name}</p>
+        <p className={css.number}>{number}</p>
+      </div>
+      <button className={css.btn}>Delete{id}</button>
     </div>
   );
 };
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,10 +1,11 @@
 import { ContactListItem } from './Contact';
+import css from './ContactList.module.css';
 
 export const ContactList = ({ contactList }) => {
   return (
-    <ul className="list">
+    <ul className={[css['contact-list']]}>
       {contactList.map(({ id, ...contactList }) => (
-        <li className="listItem" key={id}>
+        <li className={css.listItem} key={id}>
           <ContactListItem {...contactList} />
         </li>
       ))}
